b930c4236857f98476eefbd87aff1842d17c745b
RicardoVergara12 <00160625@uca.edu.sv>
2025-07-03 21:08:44 -0700

    Agregando  Parte Programador 2 tambor de arma

--- RuletaRusa/main.cpp
+++ RuletaRusa/main.cpp
@@ -8,6 +8,56 @@
 
 using namespace std;
 
+//--------------Programador 2------------------------------.
+// Configuracion del tambor(variables globales).
+int TAMANO_BARRIL = 6;    // Tamaño del barril de la pistola.
+int VACIO = 0;            // Valor que representa cuando no hay bala presente en la recamara.
+int BALA = 1;             // Valor que representa cuando hay bala presente en la recamara.
+
+int barril[6];            // Arreglo que representa el tambor(6 disparos).
+int posicionActual = 0;   // Posición actual del tambor (camara alineada al tambor).
+
+//  FUNCIÓN 1: Verifica si hay al menos una bala en el barril
+bool hayBalaEnBarril() {
+    bool encontrada = false;
+    for (int i = 0; i < TAMANO_BARRIL; i++) {
+        if (barril[i] == BALA) {
+            encontrada = true; 
+        }
+    }
+
+    if (encontrada) {
+        return true; //Encontro una bala 
+    } else {
+        return false; 
+    }
+}
+
+
+// FUNCIÓN 2: Recarga la pistola solo si está vacía.
+void recargarSiVacio() {
+    if (hayBalaEnBarril()) return;  // Si ya hay bala, no hace nada.
+
+    // Vacía todas las cámaras
+    for (int i = 0; i < TAMANO_BARRIL; i++) {
+        barril[i] = VACIO;
+    }
+
+    // Coloca una única bala en una posición aleatoria
+    int pos = rand() % TAMANO_BARRIL;
+    barril[pos] = BALA;
+}
+
+// FUNCIÓN 3: Gira el tambor y dispara
+int girarYDisparar() {
+    posicionActual = rand() % TAMANO_BARRIL;        // Gira y elige un espacio de la recamara del 1 al 6 
+    int bala = barril[posicionActual];              // Lee si hay bala
+    barril[posicionActual] = VACIO;                 // Cámara disparada se vacía
+    posicionActual = (posicionActual + 1) % TAMANO_BARRIL;  // Avanza cámara (opcional)
+    return bala;  // 1 = BANG (hubo bala), 0 = CLICK (vacío)
+}
+//------------------------Programador 2------------------------.
+
 int main() {
     srand(time(0));
     Jugador jugador1, jugador2;
